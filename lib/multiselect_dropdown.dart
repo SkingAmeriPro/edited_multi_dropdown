0b22f3b56d8b07d07426d5736bd021e2c71965d5
SkingAmeriPro <129881621+SkingAmeriPro@users.noreply.github.com>
2023-12-11 10:11:43 -0600

    testing

--- lib/multiselect_dropdown.dart
+++ lib/multiselect_dropdown.dart
@@ -862,8 +862,16 @@ class _MultiSelectDropDownState<T> extends State<MultiSelectDropDown<T>> {
                                     ),
                                     onPressed: () {
                                       setState(() {
+                                        for (var i = 0;
+                                            i < options.length;
+                                            i++) {
+                                          _controller!
+                                              .addSelectedOption(options[i]);
+                                        }
                                         _selectedOptions.clear();
                                         selectedOptions.clear();
+                                        _controller!.selectedOptions
+                                            .addAll(options);
                                         selectedOptions.addAll(_options);
                                         _selectedOptions.addAll(
                                             _controller!.selectedOptions);
